7db4bb6eef9a2cfe7d6a8e7a6285dc0ea4f1800e
Biels25 <ga.eduardo.correa@gmail.com>
2024-10-07 21:00:38 -0300

    BackCorrigido

--- BackEnd/flask_SqLite_DBeaver/app.py
+++ BackEnd/flask_SqLite_DBeaver/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, jsonify, request
 from database import db
+from flask_cors import CORS
 from models import User, Order
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///deliveries.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
+
+CORS(app)
 
 with app.app_context():
     db.create_all()  # Cria as tabelas no banco de dados
@@ -22,7 +24,6 @@ def register_user():
     db.session.commit()
     return jsonify({"message": "User registered successfully"}), 201
 
-
 @app.route('/orders', methods=['POST'])
 def create_order():
     data = request.get_json()
@@ -37,6 +38,17 @@ def get_order(order_id):
     if order:
         return jsonify({'id': order.id, 'status': order.status, 'address': order.address})
     return jsonify({'message': 'Order not found'}), 404
+
+@app.route('/orders/all', methods=['GET'])
+def get_all_orders():
+    orders = Order.query.all()  # Pega todas as ordens do banco de dados
+    orders_list = [{
+        'id': order.id,
+        'client_id': order.client_id,
+        'address': order.address,
+        'status': order.status
+    } for order in orders]  # Cria uma lista de dicionários para cada ordem
+    return jsonify(orders_list), 200  # Retorna a lista de ordens em formato JSON
 
 if __name__ == '__main__':
     app.run(debug=True)
