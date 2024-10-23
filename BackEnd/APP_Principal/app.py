aab4bff304b1c04dbf94227d8d2bb6be6134168d
Biels25 <ga.eduardo.correa@gmail.com>
2024-10-23 19:20:08 -0300

    Carrinho de compras funcionando

--- BackEnd/APP_Principal/app.py
+++ BackEnd/APP_Principal/app.py
@@ -5,26 +5,21 @@ from models import User, Order, Motoboy
 import os
 import json
 
-
 app = Flask(__name__)
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "database/deliveries.db")}'
-
 db.init_app(app)
 
-# Configurando CORS para permitir requisições de qualquer origem
-CORS(app, resources={r"/*": {"origins": "*"}}) 
+# Configurando CORS
+CORS(app)
 
-
-UPLOAD_FOLDER = os.path.join(os.getcwd(), r'BackEnd\APP_Principal\uploads\CopiaCNH')
-UPLOAD_FOLDER1 = os.path.join(os.getcwd(), r'BackEnd\APP_Principal\uploads\DocumentoDaMoto')
-UPLOAD_FOLDER2 = os.path.join(os.getcwd(), r'BackEnd\APP_Principal\uploads\FotoDoRosto')
-UPLOAD_FOLDER3 = os.path.join(os.getcwd(), r'BackEnd\APP_Principal\uploads\FotoMoto(ComPlaca)')
-
-print(os.getcwd())
-
-
+# Diretórios de upload
+UPLOAD_FOLDERS = {
+    'file1': os.path.join(os.getcwd(), r'BackEnd\APP_Principal\uploads\FotoDoRosto'),
+    'file2': os.path.join(os.getcwd(), r'BackEnd\APP_Principal\uploads\CopiaCNH'),
+    'file3': os.path.join(os.getcwd(), r'BackEnd\APP_Principal\uploads\FotoMoto(ComPlaca)'),
+    'file4': os.path.join(os.getcwd(), r'BackEnd\APP_Principal\uploads\DocumentoDaMoto'),
+}
 
 with app.app_context():
     db.create_all()  # Cria as tabelas no banco de dados
@@ -35,19 +30,13 @@ def login_user():
     if not data or 'username' not in data or 'password' not in data:
         return jsonify({"message": "Invalid input"}), 400
 
-    user = User.query.filter_by(username=data['username']).first()
-    
-    if user and user.password == data['password']:  # Aqui você deve usar uma verificação segura de senha
-        return jsonify({"message": "Login successful", "user_id": user.id}), 200
-    
-    motoboy = Motoboy.query.filter_by(username=data['username']).first()
+    user = User.query.filter_by(username=data['username']).first() or \
+           Motoboy.query.filter_by(username=data['username']).first()
 
-    if motoboy and motoboy.password == data['password']:
-        return jsonify({"message": "Login successful", "motoboy_id": motoboy.id}), 200
+    if user and user.password == data['password']:
+        return jsonify({"message": "Login successful", f"{type(user).__name__.lower()}_id": user.id}), 200
     
-    else:
-        return jsonify({"message": "Invalid username or password"}), 401
-
+    return jsonify({"message": "Invalid username or password"}), 401
 
 @app.route('/register', methods=['POST'])
 def register_user():
@@ -55,68 +44,39 @@ def register_user():
     if not data or 'username' not in data or 'password' not in data or 'email' not in data or 'cpf' not in data:
         return jsonify({"message": "Invalid input"}), 400
 
-    new_user = User(
-        username=data['username'],
-        password=data['password'],
-        email=data['email'],
-        cpf=data['cpf']
-    )
+    new_user = User(**data)
     db.session.add(new_user)
     db.session.commit()
     return jsonify({"message": "Usuário registrado com sucesso!"}), 201
 
 @app.route('/motoboys/register', methods=['POST'])
 def register_motoboy():
-    # Verifica se há arquivos na requisição
-    if 'file1' not in request.files or 'file2' not in request.files or \
-       'file3' not in request.files or 'file4' not in request.files:
+    if not all(file in request.files for file in UPLOAD_FOLDERS):
         return jsonify({"message": "Todos os arquivos são obrigatórios"}), 400
 
-    # Extrai os dados do cadastro do FormData
     cadastro_data = request.form.get('cadastro')
     if not cadastro_data:
         return jsonify({"message": "Dados do cadastro são obrigatórios"}), 400
 
-    cadastro = json.loads(cadastro_data)  # Converte a string JSON de volta para um dicionário
-
-    # Verifica se todos os campos necessários estão presentes
+    cadastro = json.loads(cadastro_data)
     if not all(key in cadastro for key in ['username', 'password', 'email', 'cpf']):
         return jsonify({"message": "Dados inválidos"}), 400
 
-    new_motoboy = Motoboy(
-        username=cadastro['username'],
-        password=cadastro['password'],
-        email=cadastro['email'],
-        cpf=cadastro['cpf']
-    )
-    
-    db.session.add(new_motoboy)
+    new_motoboy = Motoboy(**cadastro)
+    db.sess ion.add(new_motoboy)
     db.session.commit()
 
-    # Faz o upload das imagens
-    file = request.files['file1']
-    file_path = os.path.join(UPLOAD_FOLDER2, new_motoboy.username + 'file1' + ".jpg")
-    file.save(file_path)
-
-    file = request.files['file2']
-    file_path = os.path.join(UPLOAD_FOLDER, new_motoboy.username + 'file2' + ".jpg")
-    file.save(file_path)
-
-    file = request.files['file3']
-    file_path = os.path.join(UPLOAD_FOLDER3, new_motoboy.username + 'file3' + ".jpg")
-    file.save(file_path)
-
-    file = request.files['file4']
-    file_path = os.path.join(UPLOAD_FOLDER1, new_motoboy.username + 'file4' + ".jpg")
-    file.save(file_path)
+    for file_key in UPLOAD_FOLDERS:
+        file = request.files[file_key]
+        file_path = os.path.join(UPLOAD_FOLDERS[file_key], f"{new_motoboy.username}{file_key}.jpg")
+        file.save(file_path)
 
     return jsonify({"message": "Motoboy registrado com sucesso!"}), 201
-
 
 @app.route('/orders', methods=['POST'])
 def create_order():
     data = request.get_json()
-    new_order = Order(client_id=data['client_id'], address=data['address'])
+    new_order = Order(**data)
     db.session.add(new_order)
     db.session.commit()
     return jsonify({'message': 'Ordem criada com sucesso'}), 201
@@ -124,37 +84,27 @@ def create_order():
 @app.route('/orders/<int:order_id>', methods=['GET'])
 def get_order(order_id):
     order = Order.query.get(order_id)
-    if order:
-        return jsonify({'id': order.id, 'status': order.status, 'address': order.address})
-    return jsonify({'message': 'Ordem não encontrada'}), 404
+    return jsonify({'id': order.id, 'status': order.status, 'address': order.address}) if order else \
+           jsonify({'message': 'Ordem não encontrada'}), 404
 
 @app.route('/orders/all', methods=['GET'])
 def get_all_orders():
-    orders = Order.query.all()  # Pega todas as ordens do banco de dados
-    orders_list = [{
-        'id': order.id,
-        'client_id': order.client_id,
-        'address': order.address,
-        'status': order.status
-    } for order in orders]  # Cria uma lista de dicionários para cada ordem
-    return jsonify(orders_list), 200  # Retorna a lista de ordens em formato JSON
-
-
+    orders = Order.query.all()
+    orders_list = [{'id': order.id, 'client_id': order.client_id, 'address': order.address, 'status': order.status} for order in orders]
+    return jsonify(orders_list), 200
 
 @app.route('/upload', methods=['POST'])
-def success():
-    if request.method == 'POST':
-        f = request.files['file']
-        file_path = os.path.join(UPLOAD_FOLDER, f.filename)
-        f.save(file_path)
-        return "sucesso"
-    
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({"message": "No file part"}), 400
+    f = request.files['file']
+    file_path = os.path.join(UPLOAD_FOLDERS['file2'], f.filename)
+    f.save(file_path)
+    return jsonify({"message": "Upload bem-sucedido"}), 201
+
 @app.route('/getImage/<filename>', methods=['GET'])
 def get_file(filename):
-    return send_from_directory(UPLOAD_FOLDER, filename)
+    return send_from_directory(UPLOAD_FOLDERS['file2'], filename)
 
-
-
-if __name__ == '__main__':  
-    app.run(host= '0.0.0.0', debug = True)  
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
--- teste/bruno/catalogo/loja.js
+++ teste/bruno/catalogo/loja.js
@@ -1,71 +1,68 @@
 if (document.readyState == 'loading') {
-  document.addEventListener('DOMContentLoaded', ready)
+  document.addEventListener('DOMContentLoaded', ready);
 } else {
-  ready()
+  ready();
 }
 
-var totalAmount = "0,00"
+let totalAmount = "0,00";
 
 function ready() {
   // Botão remover produto
-  const removeCartProductButtons = document.getElementsByClassName("remove-product-button")
-  for (var i = 0; i < removeCartProductButtons.length; i++) {
-    removeCartProductButtons[i].addEventListener("click", removeProduct)
+  const removeCartProductButtons = document.getElementsByClassName("remove-product-button");
+  for (let i = 0; i < removeCartProductButtons.length; i++) {
+    removeCartProductButtons[i].addEventListener("click", removeProduct);
   }
 
   // Mudança valor dos inputs
-  const quantityInputs = document.getElementsByClassName("product-qtd-input")
-  for (var i = 0; i < quantityInputs.length; i++) {
-    quantityInputs[i].addEventListener("change", checkIfInputIsNull)
+  const quantityInputs = document.getElementsByClassName("product-qtd-input");
+  for (let i = 0; i < quantityInputs.length; i++) {
+    quantityInputs[i].addEventListener("change", checkIfInputIsNull);
   }
 
   // Botão add produto ao carrinho
-  const addToCartButtons = document.getElementsByClassName("button-hover-background")
-  for (var i = 0; i < addToCartButtons.length; i++) {
-    addToCartButtons[i].addEventListener("click", addProductToCart)
+  const addToCartButtons = document.getElementsByClassName("add-to-cart");
+  for (let i = 0; i < addToCartButtons.length; i++) {
+    addToCartButtons[i].addEventListener("click", addProductToCart);
   }
 
   // Botão comprar
-  const purchaseButton = document.getElementsByClassName("purchase-button")[0]
-  purchaseButton.addEventListener("click", makePurchase)
+  const purchaseButton = document.getElementsByClassName("purchase-button")[0];
+  purchaseButton.addEventListener("click", makePurchase);
 }
 
 function removeProduct(event) {
-  event.target.parentElement.parentElement.remove()
-  updateTotal()
+  event.target.parentElement.parentElement.remove();
+  updateTotal();
 }
 
 function checkIfInputIsNull(event) {
   if (event.target.value === "0") {
-    event.target.parentElement.parentElement.remove()
+    event.target.parentElement.parentElement.remove();
   }
-
-  updateTotal()
+  updateTotal();
 }
 
 function addProductToCart(event) {
-  const button = event.target
-  const productInfos = button.parentElement.parentElement
-  const productImage = productInfos.getElementsByClassName("product-image")[0].src
-  const productName = productInfos.getElementsByClassName("product-title")[0].innerText
-  const productPrice = productInfos.getElementsByClassName("product-price")[0].innerText
+  const button = event.target;
+  const productInfos = button.closest('.card-item');
+  const productName = productInfos.getElementsByClassName("product-title")[0].innerText;
+  const productPrice = productInfos.getElementsByClassName("product-price")[0].innerText;
 
-  const productsCartNames = document.getElementsByClassName("cart-product-title")
-  for (var i = 0; i < productsCartNames.length; i++) {
+  const productsCartNames = document.getElementsByClassName("cart-product-title");
+  for (let i = 0; i < productsCartNames.length; i++) {
     if (productsCartNames[i].innerText === productName) {
-      productsCartNames[i].parentElement.parentElement.getElementsByClassName("product-qtd-input")[0].value++
-      updateTotal()
-      return
+      productsCartNames[i].parentElement.parentElement.getElementsByClassName("product-qtd-input")[0].value++;
+      updateTotal();
+      return;
     }
   }
 
-  let newCartProduct = document.createElement("tr")
-  newCartProduct.classList.add("cart-product")
+  let newCartProduct = document.createElement("tr");
+  newCartProduct.classList.add("cart-product");
 
   newCartProduct.innerHTML =
     `
       <td class="product-identification">
-        <img src="${productImage}" alt="${productName}" class="cart-product-image">
         <strong class="cart-product-title">${productName}</strong>
       </td>
       <td>
@@ -75,46 +72,29 @@ function addProductToCart(event) {
         <input type="number" value="1" min="0" class="product-qtd-input">
         <button type="button" class="remove-product-button">Remover</button>
       </td>
-    `
-  
-  const tableBody = document.querySelector(".cart-table tbody")
-  tableBody.append(newCartProduct)
-  updateTotal()
+    `;
 
-  newCartProduct.getElementsByClassName("remove-product-button")[0].addEventListener("click", removeProduct)
-  newCartProduct.getElementsByClassName("product-qtd-input")[0].addEventListener("change", checkIfInputIsNull)
+  const tableBody = document.querySelector(".cart-table tbody");
+  tableBody.append(newCartProduct);
+  updateTotal();
+
+  newCartProduct.getElementsByClassName("remove-product-button")[0].addEventListener("click", removeProduct);
+  newCartProduct.getElementsByClassName("product-qtd-input")[0].addEventListener("change", checkIfInputIsNull);
 }
 
-function makePurchase() {
-  if (totalAmount === "0,00") {
-    alert("Seu carrinho está vazio!")
-  } else {   
-    alert(
-      `
-        Obrigado pela sua compra!
-        Valor do pedido: R$${totalAmount}\n
-        Volte sempre :)
-      `
-    )
 
-    document.querySelector(".cart-table tbody").innerHTML = ""
-    updateTotal()
-  }
-}
-
-// Atualizar o valor total do carrinho
 function updateTotal() {
-  const cartProducts = document.getElementsByClassName("cart-product")
-  totalAmount = 0
+  const cartProducts = document.getElementsByClassName("cart-product");
+  totalAmount = 0;
 
-  for (var i = 0; i < cartProducts.length; i++) {
-    const productPrice = cartProducts[i].getElementsByClassName("cart-product-price")[0].innerText.replace("R$", "").replace(",", ".")
-    const productQuantity = cartProducts[i].getElementsByClassName("product-qtd-input")[0].value
+  for (let i = 0; i < cartProducts.length; i++) {
+    const productPrice = parseFloat(cartProducts[i].getElementsByClassName("cart-product-price")[0].innerText.replace("R$", "").replace(",", "."));
+    const productQuantity = parseInt(cartProducts[i].getElementsByClassName("product-qtd-input")[0].value);
 
-    totalAmount += productPrice * productQuantity
+    totalAmount += productPrice * productQuantity;
   }
-  
-  totalAmount = totalAmount.toFixed(2)
-  totalAmount = totalAmount.replace(".", ",")
-  document.querySelector(".cart-total-container span").innerText = "R$" + totalAmount
+
+  totalAmount = totalAmount.toFixed(2);
+  totalAmount = totalAmount.replace(".", ",");
+  document.querySelector(".cart-total-container span").innerText = "R$" + totalAmount;
 }
